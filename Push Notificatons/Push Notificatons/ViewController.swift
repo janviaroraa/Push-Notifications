8ff81c5b50f5b9b1ddc7f6a5fca06fd38e5c6094
Janvi Arora <jjanvi.arora18@gmail.com>
2023-05-04 12:29:12 +0530

    push notifications project set up

---
+++ Push Notificatons/Push Notificatons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Push Notificatons
+//
+//  Created by Powerplay on 04/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
